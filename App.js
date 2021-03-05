cfd26a7be81d29a1f93c7fab124b13d87d4e88db
m.veen <mike-veen@hotmail.com>
2021-03-06 00:07:17 +0100

    Base- initial commit

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+//Zo gebruik je externe style sheets
+var styles = require('./styleSheets/number1');
+
+//Deep Pruple ~ #7c00ff || Dark red ~ #700c0c ||  Royal pruple ~ 7851a9 || Lighter red ~ #b93737 ||
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.mainContainer}>
+      <LinearGradient colors={['#7851a9', '#b93737']} style={styles.mainContainer}>
+
+        <View style={styles.ImageContainer}>
+          <ImageBackground source={require('./assets/1.jpg')} style={styles.image}/>
+        </View>
+
+        <View style={styles.titelContainer}>
+          <Text style={styles.titel}>
+            Hier komt de muziek titel & artiest
+          </Text>
+        </View>
+
+        <View style={styles.ctrlContainer}>
+          <Text style={styles.slider}>Hier komt de slider</Text>
+          <Text style={styles.beginCount}>0:00</Text>
+          <Text style={styles.endCount}>3:56</Text>
+        </View>
+        </LinearGradient>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styleSheets/number1.js
@@ -0,0 +1,66 @@
+import React, { Component, PropTypes } from 'react';
+import { StyleSheet } from 'react-native';
+
+var styles = StyleSheet.create({
+    mainContainer: {
+      flex: 1,
+      backgroundColor: 'black',
+      minHeight: '100%'
+    },
+    ImageContainer: {
+      width: 200,
+      height: 200,
+      alignSelf: 'center',
+      marginTop: '15%',
+    },
+    image: {
+      flex: 1,
+      resizeMode: "contain",
+      justifyContent: "center",
+      borderRadius: 150,
+      borderWidth: 1,
+      overflow: 'hidden',
+    },
+  
+    titelContainer: {
+      backgroundColor: 'rgba(94, 64, 226, 1)',
+      alignSelf: 'center',
+      width: '80%',
+      height: '5%',
+      margin: '5%',
+      justifyContent: 'center',
+    },
+    titel: {
+      color: '#fff',
+      alignSelf: 'center',
+    },
+
+    ctrlContainer: {
+      backgroundColor: 'green',
+      alignSelf: 'center',
+      width: '90%',
+      height: '20%',
+    },
+    slider: {
+        alignSelf: 'center',
+        color: '#fff',
+        margin: '2%',
+        backgroundColor: 'red',
+        width: '100%',
+        fontSize: 20,
+    },
+
+    beginCount: {
+        color: '#fff',
+        position: 'absolute',
+        top: '20%',
+    },
+    endCount: {
+        color: '#fff',
+        alignSelf: 'flex-end',
+        position: 'absolute',
+        top: '20%',
+    }
+});
+
+module.exports = styles;
